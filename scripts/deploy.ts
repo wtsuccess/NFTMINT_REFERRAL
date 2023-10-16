8d7024c2a356ca1d73b155427ac20fc9e0625294
wtsuccess <white991120@gmail.com>
2023-10-16 17:14:59 -0400

    change script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,8 +2,19 @@ import { ethers } from "hardhat";
 
 async function main() {
   const NFTReferal = await ethers.getContractFactory("NFTReferal");
-  const baseTokenURI = "ipfs://QmZbWNKJPAjxXuNFSEaksCJVd1M6DaKQViJBYPK2BdpDEP/";
-  const nftReferal = await NFTReferal.deploy(true, 100, 500, 0.1, baseTokenURI);
+  const baseTokenURI = "";
+  const paymentEngineAdd = "";
+  const GS50Address = "";
+  const nftReferal = await NFTReferal.deploy(
+    true,
+    100,
+    ethers.utils.parseEther("0.1"),
+    baseTokenURI,
+    1000,
+    10000,
+    paymentEngineAdd,
+    GS50Address
+  );
   await nftReferal.deployed();
   console.log("NFTReferal address", nftReferal.address);
 }
